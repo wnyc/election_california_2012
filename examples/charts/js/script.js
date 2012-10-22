00014182bc931b1bd977c39f0e60ce4533361664
Steven Melendez <smelendez@wnyc.org>
2012-10-22 12:53:35 -0400

    Commas in vote totals; coloring on us.senate,us.president

--- examples/charts/js/script.js
+++ examples/charts/js/script.js
@@ -1,5 +1,4 @@
-
-    var config;
+var config;
 
 $(document).ready(function(){
     var statewide_contest_template = Handlebars.compile($("#statewide-contest-template").html());
@@ -10,12 +9,28 @@ $(document).ready(function(){
     var proposition_results_template = Handlebars.compile($("#proposition-results-template").html());
     var election;
     var router;
+
+    var REP_HI = "#f40b0b", REP_MED = "#ff6666", REP_LO = "#ffb0b0", DEM_HI = "#b6ecff", DEM_MED = "#6cceff", DEM_LO = "#009eff";
     
     var presidential_view, ussenate_view, ushouse_view, casenate_view, caassembly_view, propositions_view;
     var county_map_view, assembly_map_view, ushouse_map_view, casenate_map_view;
     Handlebars.registerHelper('result_table_template', result_table_template);
     Handlebars.registerHelper('county_results_template', county_results_template);
+    Handlebars.registerHelper('add_commas', addCommas);
 
+    function addCommas(nStr)
+    {
+        // Public domain code from http://www.mredkj.com/javascript/nfbasic.html
+        nStr += '';
+        x = nStr.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        }
+        return x1 + x2;
+    }
     var Config = Backbone.Model.extend({
         // body, contest, county
         redraw_features : function () {
@@ -128,7 +143,7 @@ $(document).ready(function(){
                 // Remove third-party candidates
                 // Consolidating them as other
 
-                candidates.remove(candidates.rest(3));
+                candidates.remove(candidates.filter(function(cand){return cand.get("party") != 'Dem' && cand.get("party") != 'Rep' && cand.get("party") != '---';}));
                 the_contests.add(newcontest);
             });
             the_body.set("contests", the_contests);
@@ -248,7 +263,7 @@ $(document).ready(function(){
         }
         
 
-        return newcandidates.remove(newcandidates.rest(3));
+        return newcandidates.remove(candidates.filter(function(cand){return cand.get("party") != 'Dem' && cand.get("party") != 'Rep' && cand.get("party") != '---';}));
 
     }
     var Candidate = Backbone.Model.extend({
@@ -413,7 +428,92 @@ $(document).ready(function(){
 
         }
 
-        return {fillColor: "#999", fillOpacity: 0.7, strokeWidth: 1,visible: true };
+
+        // Red blue map
+        if (config.get("county"))
+        {
+            // If another county is selected
+            return {fillColor: "#999", fillOpacity: 0.7, strokeWidth: 1,visible: true };
+        }
+
+        else
+        {
+            var body = config.get("body");
+            var contest = election.find(function(b){return b.get("name") == body}).get("contests").first(); // Only one for these statewide offices
+            var thecounty = this.id;
+            var county = contest.get("counties").find(function(c){
+                return c.get("title") == thecounty;
+            });
+            if (county.get("precincts_reporting_percent") < 10)
+            {
+                // Insufficent data -- color it grey
+                return {fillColor: "#999", fillOpacity: 0.7, strokeWidth: 1,visible: true };
+            }
+
+
+            var dem = county.get("candidates").find(function(c){return c.get("party") == "Dem"});
+            var rep = county.get("candidates").find(function(c){return c.get("party") == "Rep"});
+
+            if (_.isUndefined(dem))
+            {
+                dem_percent = 0;
+            }
+            else
+            {
+                dem_percent = dem.get("vote_percent");
+            }
+            if (_.isUndefined(rep))
+            {
+                rep_percent = 0;
+            }
+            else
+            {
+                rep_percent = rep.get("vote_percent");
+            }
+
+            if (rep_percent > 80)
+            {
+                return {fillColor: REP_HI, fillOpacity: 0.7, strokeWidth: 1,visible: true };
+            }
+            else if (rep_percent > 60)
+            {
+                return {fillColor: REP_MED, fillOpacity: 0.7, strokeWidth: 1,visible: true };
+            }
+            else if (rep_percent > 50)
+            {
+                return {fillColor: REP_LO, fillOpacity: 0.7, strokeWidth: 1,visible: true };
+            }
+
+            else if (dem_percent > 80)
+            {
+                return {fillColor: DEM_HI, fillOpacity: 0.7, strokeWidth: 1,visible: true };
+            }
+            else if (dem_percent > 60)
+            {
+                return {fillColor: DEM_MED, fillOpacity: 0.7, strokeWidth: 1,visible: true };
+            }
+            else if (dem_percent > 50)
+            {
+                return {fillColor: DEM_LO, fillOpacity: 0.7, strokeWidth: 1,visible: true };
+            }
+            else
+            {
+                return {fillColor: "#999", fillOpacity: 0.7, strokeWidth: 1,visible: true };
+            }
+
+
+
+
+
+
+                
+
+            
+
+        }
+
+
+
     }
     function county_responsive_highlighted_opts () {
         if (!config.get("showcounties"))
@@ -565,12 +665,12 @@ $(document).ready(function(){
            "body/:body/:contest/:county" : "navto"
         },
         navto: function(body, contest, county) {
-            config.set({body : body || 'presidential', contest: contest || '1', county : county || '' }, {silent: true});
+            config.set({body : body || "us.president", contest: contest || '1', county : county || '' }, {silent: true});
             this.show (body, contest, county);
         },
 
         show: function(body, contest, county) {
-            if (body == "presidential")
+            if (body == "us.president")
             {
                 presidential_view.render(county);
                 config.set({
@@ -583,7 +683,7 @@ $(document).ready(function(){
                 });
 
             }
-            if (body == "ussenate")
+            if (body == "us.senate")
             {
                 ussenate_view.render(county);
                 config.set({
@@ -595,7 +695,7 @@ $(document).ready(function(){
 
                 });
             }
-            else if (body == "ushouse")
+            else if (body == "us.house")
             {
                 ushouse_view.render(contest);
                 config.set({
@@ -607,7 +707,7 @@ $(document).ready(function(){
 
                 });
             }
-            else if (body == "casenate")
+            else if (body == "ca.senate")
             {
                 casenate_view.render(contest);
                 config.set({
@@ -644,7 +744,7 @@ $(document).ready(function(){
                 });
             }
             else {
-                config.set({body : 'presidential'});
+                config.set({body : "us.president"});
                 config.set({
                     showcounties: true,
                     showassembly: false,
@@ -699,7 +799,7 @@ $(document).ready(function(){
         if(!Backbone.history.start())
         {
             // By default start with presidential with no specific county
-            config.set({body: 'presidential'});
+            config.set({body: "us.president"});
         }
 
 
